1785200074354a1362e2aa2f3369e7f53f9ac5e2
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-20 15:15:47 +0900

    Update: userRole 받아서 저장, registeredAt 필드 현재 시간으로 저장

--- src/main/java/com/spring/snsproject/domain/dto/UserJoinRequest.java
+++ src/main/java/com/spring/snsproject/domain/dto/UserJoinRequest.java
@@ -1,8 +1,11 @@
 package com.spring.snsproject.domain.dto;
 
+import com.spring.snsproject.domain.UserRole;
 import com.spring.snsproject.domain.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.sql.Timestamp;
 
 @AllArgsConstructor
 @Getter
@@ -10,10 +13,12 @@ public class UserJoinRequest {
     private String userName;
     private String password;
 
-    public User toEntity(String encodedPassword){
+    public User toEntity(String encodedPassword, UserRole role){
         return User.builder()
                 .userName(this.userName)
                 .password(encodedPassword)
+                .role(role)
+                .registeredAt(new Timestamp(System.currentTimeMillis()))
                 .build();
     }
 }
