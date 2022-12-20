8a84e6bf53d4b4602486805e7a9a58737c915494
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-20 16:16:12 +0900

    Example Controller

---
+++ src/main/java/com/spring/snsproject/controller/ExampleController.java
@@ -0,0 +1,20 @@
+package com.spring.snsproject.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ExampleController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok().body("hello");
+    }
+    @GetMapping("/bye")
+    public ResponseEntity<String> bye(){
+        return ResponseEntity.ok().body("bye");
+    }
+}
