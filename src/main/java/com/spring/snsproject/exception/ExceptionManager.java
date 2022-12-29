f6a1e2afd58c30e1a86bd0081bf0f84408e0c3ae
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-29 17:41:26 +0900

    리팩토링 오타 수정 - SQLException을 ExceptionManger에서 처리

--- src/main/java/com/spring/snsproject/exception/ExceptionManager.java
+++ src/main/java/com/spring/snsproject/exception/ExceptionManager.java
@@ -23,7 +23,7 @@ public class ExceptionManager {
                 .body(Response.error(new ErrorResponse(e.getErrorCode().getHttpStatus().name(), e.toString())));
     }
 
-    @ExceptionHandler(AppException.class)
+    @ExceptionHandler(SQLException.class)
     public ResponseEntity<?> sqlExceptionHandler(SQLException e){
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(Response.error(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.name(), "DATABASE ERROR")));
