c737c8a2acf2f9cee81a4ad2a12c9e1a69ba25ed
Jiyoung <ahn.jiyoung15@gmail.com>
2022-12-29 10:04:03 +0900

    sumOfDigit

--- src/main/java/com/spring/snsproject/controller/ExampleController.java
+++ src/main/java/com/spring/snsproject/controller/ExampleController.java
@@ -2,6 +2,7 @@ package com.spring.snsproject.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,15 @@ public class ExampleController {
     @GetMapping("/hello")
     public ResponseEntity<String> hello(){
         return ResponseEntity.ok().body("안지영");
+    }
+    @GetMapping("/hello/{num}")
+    public ResponseEntity sumOfDigit(@PathVariable int num){
+        int sum = 0;
+        while(num!=0){
+            sum += num % 10;
+            num = (int) num / 10;
+        }
+        return ResponseEntity.ok().body(sum);
     }
     @GetMapping("/bye")
     public ResponseEntity<String> bye(){
