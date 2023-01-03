78f8d84f033dcfa1ece04e6a46b6d0aa3a9a12b1
Jiyoung <ahn.jiyoung15@gmail.com>
2023-01-03 17:57:14 +0900

    마이피드 기능 구현 - 인증 필요

--- src/main/java/com/spring/snsproject/repository/PostRepository.java
+++ src/main/java/com/spring/snsproject/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package com.spring.snsproject.repository;
 
 import com.spring.snsproject.domain.entity.Post;
+import com.spring.snsproject.domain.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findAll(Pageable pageable);
+
+    Page<Post> findByUser(User user, Pageable pageable);
 }
