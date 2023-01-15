ef7d1148b80164723800b092bc7f94be5e7346d4
Jiyoung <ahn.jiyoung15@gmail.com>
2023-01-15 23:43:46 +0900

    포스트 컨트롤러 - 포스트 수정, 삭제

--- src/main/java/com/spring/snsproject/controller/PostController.java
+++ src/main/java/com/spring/snsproject/controller/PostController.java
@@ -1,17 +1,20 @@
 package com.spring.snsproject.controller;
 
+import com.spring.snsproject.domain.dto.post.PostEditRequest;
 import com.spring.snsproject.domain.dto.post.PostGetResponse;
+import com.spring.snsproject.domain.dto.post.PostResponse;
+import com.spring.snsproject.domain.dto.post.PostWriteRequest;
+import com.spring.snsproject.domain.entity.Post;
 import com.spring.snsproject.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/posts")
@@ -39,6 +42,35 @@ public class PostController {
         PostGetResponse post = postService.getOne(postId);
         model.addAttribute("post", post);
         return "posts/show";
+    }
+
+    @GetMapping("/new")
+    public String write(){
+        return "posts/new";
+    }
+
+    @PostMapping ("/new")
+    public String postNew(PostWriteRequest postWriteRequest, Authentication authentication, Model model){
+        postService.write(postWriteRequest, authentication.getName());
+        return "redirect:/posts/list";
+    }
+
+    @GetMapping("/{postId}/edit")
+    public String edit(@PathVariable Long postId, Model model){
+        model.addAttribute("postId", postId);
+        return "posts/edit";
+    }
+
+    @PutMapping("/{postId}")
+    public String modify(@PathVariable Long postId, PostEditRequest postEditRequest, Authentication authentication){
+        postService.edit(postId, postEditRequest, authentication.getName());
+        return "redirect:/posts/list";
+    }
+
+    @DeleteMapping("/{postId}")
+    public String delete(@PathVariable Long postId, Authentication authentication){
+        postService.delete(postId, authentication.getName());
+        return "redirect:/posts/list";
     }
 }
 
